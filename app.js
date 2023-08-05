89343fe5f144fc97b1ff4a97def2336b3d73ea88
Murod <137706664+themurod@users.noreply.github.com>
2023-08-05 16:40:18 +0500

    code1
    my first code

--- app.js
+++ app.js
@@ -1,121 +1,37 @@
-// canvas
-const canvas = document.getElementById('canvas');
-// canvas 2d
-const context = canvas.getContext('2d');
+let ism = prompt('ismingiz')
+let familiya = prompt('familyangiz')
+let parol = prompt('parol kiriting')
+let report = prompt('arizangizni kiriting')
+// ISM TUGRI YOZILISHI BOSHLANDI
+let boshi = ism.charAt().toUpperCase()
+let davomi = ism.slice(1).toLowerCase()
+let $ism = boshi + davomi 
+let fboshi = familiya.charAt().toUpperCase()
+let fdavomi = familiya.slice(1).toLowerCase()
+let $familiya = fboshi + fdavomi 
+// ISM TUGRI YOZILISHI TUGADI
 
-// O'yin maydonidagi kataklar o'lcham
-var grid = 16;
-var count = 0;
-var score = 0;
-var max = 0;
 
-// snake
-const snake = {
-    x: 160,
-    y: 160,
 
-    dx: grid,
-    dy: 0,
-
-    maxCells: 1,
-
-    cells: [],
+if ( parol.length > 16 || $ism.length > 18 || $ism.length < 2 ) {
+    console.log(`akkount yaratilmadi!
+    sabab: parolingiz juda uzun,
+    yoki ismda muammo bor !`)
 }
-
-const food = {
-    x: 320,
-    y: 320,
+else if (parol.length <= 8 ) {
+    console.log(`akkount yaratilmadi!
+    sabab: parolingiz juda kichik !`)
+} else {
+    console.log(`akkount muvaffaqiyatli yaratildi! 
+    
+    ism: ${$ism}
+    familiya: ${$familiya}
+    parol: ${parol} `)     
 }
-
-function getRandomInt(min, max){
-    return Math.floor(Math.random() * (max - min)) + min;
-}
-
-function loop() {
-    requestAnimationFrame(loop)
-    if(++count < 4){ // 60fps / 15fps = 4
-        return;
-    }
-
-    count = 0;
-    context.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
-
-    snake.x += snake.dx;
-    snake.y += snake.dy;
-
-    if(snake.x < 0){ // gorizontal
-        snake.x = canvas.clientWidth - grid;
-    } else if (snake.x >= canvas.clientWidth) {
-        snake.x = 0;
-    }
-
-    if(snake.y < 0){ // vertical
-        snake.y = canvas.clientHeight - grid;
-    } else if (snake.y >= canvas.clientHeight) {
-        snake.y = 0;
-    }
-
-    snake.cells.unshift({ x: snake.x, y: snake.y });
-
-    if(snake.cells.length > snake.maxCells){
-        snake.cells.pop();
-    }
-
-    // ovqat rangi
-    context.fillStyle = '#fff';
-    context.fillRect(food.x, food.y, grid - 1, grid - 1);
-
-    // snake style
-    context.fillStyle = '#E43F5A';
-
-    snake.cells.forEach(function (cell, index) {
-        context.fillRect(cell.x, cell.y, grid - 1, grid - 1);
-        if(cell.x === food.x && cell.y === food.y){
-            // ilonni uzunligi
-            snake.maxCells++;
-            // ochko
-            score += 1;
-            document.getElementById('score').innerHTML = score;
-            // ovqat
-            food.x = getRandomInt(0, 25) * grid;
-            food.y = getRandomInt(0, 25) * grid;
-        }
-        for(var i = index + 1; i < snake.cells.length; i++){
-            if(cell.x === snake.cells[i].x && cell.y === snake.cells[i].y){
-                if(score > max) {
-                    max = score;
-                }
-                snake.x = 160,
-                snake.y = 160,
-                snake.cells = [];
-                snake.maxCells = 1;
-                snake.dx = grid;
-                snake.dy = 0;
-                score = 0;
-                food.x = getRandomInt(0, 25) * grid;
-                food.y = getRandomInt(0, 25) * grid;
-                document.getElementById('score').innerHTML = max;
-            }
-        }
-    })
-}
-
-document.addEventListener('keydown', function(e) {
-    if(e.keyCode === 37 && snake.dx === 0){ // chap
-        snake.dx = -grid;
-        snake.dy = 0
-    } else if(e.keyCode === 38 && snake.dy === 0){ // tepa
-        snake.dy = -grid;
-        snake.dx = 0
-    } else if(e.keyCode === 39 && snake.dx === 0){ // o'ng
-        snake.dx = grid;
-        snake.dy = 0
-    } else if(e.keyCode === 40 && snake.dy === 0){ // past
-        snake.dy = grid;
-        snake.dx = 0
-    } else {
-        return;
-    }
-})
-
-requestAnimationFrame(loop)
+if (report.length > 30 ) {
+    console.log(`ariza: ${report}`)
+    
+} else (
+    console.log(`ariza juda ham kichik, 30 harfan ko'proq bo'lsin.
+     hozir matnda ${report.length} ta harf qatnashgan`)
+)
